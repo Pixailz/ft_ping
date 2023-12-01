39677f8a8da62c0a38081f7c8270beca5ebea624
Pixailz <Pixailz1@gmail.com>
2023-12-01 17:49:13 +0100

    flagz: readd ttl

--- src/bonus/parsing/parse.c
+++ src/bonus/parsing/parse.c
@@ -6,7 +6,7 @@
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 01:01:15 by brda-sil          #+#    #+#             */
-/*   Updated: 2023/12/01 17:37:20 by brda-sil         ###   ########.fr       */
+/*   Updated: 2023/12/01 17:43:25 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ t_bin	parse_opts(int ac, char **av)
 	ft_optadd("pattern", 'p', OPT_SINGLE);
 	ft_optadd("size", 's', OPT_SINGLE);
 	ft_optadd("tos", 'T', OPT_SINGLE);
+	ft_optadd("ttl", 0, OPT_SINGLE);
 	ft_optadd("timeout", 'w', OPT_SINGLE);
 	ft_optadd("linger", 'W', OPT_SINGLE);
 	// ft_optadd("quiet", 'q', OPT_FLAG);
